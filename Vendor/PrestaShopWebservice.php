f4b2a173100a68e3a5a010335463fcfac019354f
Dídac Rios <didac@niorcs.com>
2014-05-19 11:46:25 +0200

    removing stupid echos

--- Vendor/PrestaShopWebservice.php
+++ Vendor/PrestaShopWebservice.php
@@ -68,7 +68,6 @@ class PrestaShopWebservice
 	 * @param mixed $debug Debug mode Activated (true) or deactivated (false)
 	*/
 	function __construct($url, $key, $debug = true) {
-		echo "hola que tal";
 		if (!extension_loaded('curl'))
 		  throw new PrestaShopWebserviceException('Please activate the PHP extension \'curl\' to allow use of PrestaShop webservice library');
 		$this->url = $url;
