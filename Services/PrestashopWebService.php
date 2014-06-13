db0bac4341fe0ebf5a06b870c108004854488fc7
Dídac Rios <didac@niorcs.com>
2014-06-13 09:08:12 +0200

    Add a custom instance function
    In order to targeting multiple shops added a functiont sending the custom parameters

--- Services/PrestashopWebService.php
+++ Services/PrestashopWebService.php
@@ -6,18 +6,29 @@ class PrestashopWebService{
     private $container  = null;
     private $em         = null;
     private $instance   = null;
-    
+
     public function __construct($container){
         $this->container = $container;
         $this->em        = $container->get('doctrine')->getManager();
     }
-    
+
     public function getInstance(){
         if(is_null($this->instance)){
             $this->instance =  new \KkuetNet\PrestashopWebServiceBundle\Vendor\PrestaShopWebservice(
                 $this->container->getParameter('kkuet_net_prestashop_web_service.website'),
                 $this->container->getParameter('kkuet_net_prestashop_web_service.key'),
                 $this->container->getParameter('kkuet_net_prestashop_web_service.debug')
+            );
+        }
+        return $this->instance;
+    }
+
+    public function personalInstance($websiteurl, $websitekey, $debug) {
+        if(is_null($this->instance)){
+            $this->instance =  new \KkuetNet\PrestashopWebServiceBundle\Vendor\PrestaShopWebservice(
+                $websiteurl,
+                $websitekey,
+                $debug
             );
         }
         return $this->instance;
