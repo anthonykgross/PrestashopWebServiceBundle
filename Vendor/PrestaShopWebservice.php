e30afd851ec15f1e651baf35d47f34b8df891442
Dídac Rios <didac@niorcs.com>
2015-03-25 12:09:19 +0100

    Update PrestaShopWebservice.php

--- Vendor/PrestaShopWebservice.php
+++ Vendor/PrestaShopWebservice.php
@@ -45,7 +45,7 @@ class PrestaShopWebservice
 
 	/** @var array compatible versions of PrestaShop Webservice */
 	const psCompatibleVersionsMin = '1.4.0.17';
-	const psCompatibleVersionsMax = '1.6.0.6';
+	const psCompatibleVersionsMax = '1.6.0.14';
 
 	/**
 	 * PrestaShopWebservice constructor. Throw an exception when CURL is not installed/activated
